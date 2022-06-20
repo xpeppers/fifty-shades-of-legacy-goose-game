542d3ad4e8c6148f50850f8b30cbaa39f228b69b
Alessandro Dalfovo <dalfovo.alessandro@gmail.com>
2022-06-20 18:03:58 +0200

    Make Spark application stoppable from main

--- goose-game-two/src/main/java/com/goosegame/main.java
+++ goose-game-two/src/main/java/com/goosegame/main.java
@@ -5,21 +5,27 @@ import org.slf4j.LoggerFactory;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
+import spark.Spark;
+import spark.servlet.SparkApplication;
 
 import static spark.Spark.*;
 
 public class main {
     private static Logger logger = LoggerFactory.getLogger(main.class);
 
-    private static App app = new App();
-
     public static void main(String[] args) {
+        App app = new App();
         port(8080);
         before((req, res) -> logger.info("Request: {} {} - body: {}", req.requestMethod(), req.uri(), req.body()));
         after((req, res) -> logger.info("Response {} - body: {}", res.status(), res.body()));
 
         post("/players", (req, res) -> app.createPlayer(req, res));
         post("/players/:id/roll", (req, res) -> app.roll(req, res));
+    }
+
+    public static void stop() throws InterruptedException {
+        Spark.stop();
+        Thread.sleep(2000L);
     }
 
 }
