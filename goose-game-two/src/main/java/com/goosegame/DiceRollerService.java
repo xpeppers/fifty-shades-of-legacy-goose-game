688f8465eb55e156a2a19ff145903eb2f9fa927a
Alessandro Dalfovo <dalfovo.alessandro@gmail.com>
2022-06-20 16:59:31 +0200

    Remove external dice roller

--- goose-game-two/src/main/java/com/goosegame/DiceRollerService.java
+++ goose-game-two/src/main/java/com/goosegame/DiceRollerService.java
@@ -1,32 +1,21 @@
 package com.goosegame;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.json.JSONObject;
 
-import java.io.IOException;
+import java.util.Random;
 
 public class DiceRollerService {
 
-    private static final String DICE_SERVICE_URL = "http://roll.diceapi.com/json/d6/d6";
-
-    private final OkHttpClient httpClient;
-
     public DiceRollerService() {
-        httpClient = new OkHttpClient();
         // This service returns something like this:
         // {"success":true,"dice":[{"value":5,"type":"d6"},{"value":1,"type":"d6"}]}
     }
 
     public JSONObject roll() {
-        Request request = new Request.Builder().url(DICE_SERVICE_URL).build();
-
-        try (Response response = httpClient.newCall(request).execute()) {
-            String rollResult = response.body().string();
-            return new JSONObject(rollResult);
-        } catch (IOException e) {
-            throw new RuntimeException("cannot roll dice!", e);
-        }
+        Random random = new Random();
+        int firstDie = random.nextInt(6) + 1;
+        int secondDie = random.nextInt(6) + 1;
+        String result = "{\"success\":true,\"dice\":[{\"value\":" + firstDie + ",\"type\":\"d6\"},{\"value\":" + secondDie + ",\"type\":\"d6\"}]}";
+        return new JSONObject(result);
     }
 }
